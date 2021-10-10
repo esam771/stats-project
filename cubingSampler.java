1f6634e2983213352fb6aecd5a93ad4965d8a770
Sam <75175872+esam771@users.noreply.github.com>
2021-10-10 11:34:41 -0500

    Update cubingSampler.java

--- cubingSampler.java
+++ cubingSampler.java
@@ -1,3 +1,4 @@
+package cubingSample;
 import java.text.DecimalFormat;
 import java.util.Scanner;
 import java.io.*;
@@ -6,13 +7,8 @@ public class cubingSampler {
 
 	public static void main(String[] args) {
 		
-		int samplesize = 30;
-		int rand = 0;
-		
-		double meanTime = 0;
-		double totalTime = 0;
-		double totalDeviation = 0;
-		double stdDeviation = 0;
+		int samplesize= 30, rand = 0;
+		double meanTime = 0, totalTime = 0, totalDeviation= 0, stdDeviation = 0;
 		
 		double[] solves = new double[301];
 		double[] samples = new double[samplesize];
@@ -42,6 +38,7 @@ public class cubingSampler {
 		{
 			rand = (int)Math.ceil((300 * (Math.random()))) - 1; //random number 0-300
 			samples[i]= solves[rand];
+			System.out.println(samples[i]); //printing data values
 			totalTime = totalTime + solves[rand];
 		}
 		
@@ -57,5 +54,33 @@ public class cubingSampler {
 		System.out.println("\nStandard Deviation: " + stdDeviation+ "\n"); //printing std dev
 		
 		//solves();
+	}
+	
+	public static void solves()
+	{
+	
+		DecimalFormat decfor = new DecimalFormat("##.00");
+		
+		double meanTime = 0, totalTime = 0, totalDeviation = 0, stdDeviation = 0;
+		double[] solves = new double[223];
+		
+		for(int i = 0; i < 223; i++)
+		{
+			solves[i] = (12.87 + Math.random() * 6.64);
+			System.out.println(decfor.format(solves[i]));
+			totalTime = totalTime + solves[i];
+			
+		}
+		
+		meanTime = totalTime / 223;	
+		System.out.println("\nMean Solve Time: " + meanTime); //printing mean
+		
+		for(int x = 0; x < 223; x++) //calculating std deviation
+		{
+			totalDeviation = totalDeviation + Math.abs(solves[x] - meanTime);
+		}
+		
+		stdDeviation = totalDeviation / 223;
+		System.out.println("\nStandard Deviation: " + stdDeviation + "\n"); //printing std dev
 	}
 }
